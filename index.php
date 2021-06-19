e8d02e2fa49f7df6df9160febb993af69bfe42b2
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-06-19 23:06:32 +0500

    delete function

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php 
 
     include('config/db_connect.php');
+
+    if(isset($_POST['delete'])){
+        
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+        $sql = "DELETE FROM pizzas where id = $id_to_delete";
+
+        if(mysqli_query($conn, $sql)){
+            // header('Location: index.php');
+            echo '<script>alert("Deleted successfully!")</script>';
+        }else{
+            echo 'error query: ' . mysqli_error($conn);
+        }
+
+    }
     
     $sql = 'SELECT id, title, ingredients FROM pizzas ORDER BY created_at';
 
@@ -36,8 +51,12 @@
         
                         </ul>
                     </div>
-                    <div class="card-footer bg-white ml-auto mb-2">
-                        <a href="details.php?id=<?php echo $pizza['id']; ?>" class="text-dark text-uppercase text-muted">More Info</a>
+                    <div class="card-footer bg-white ml-auto mb-2 d-flex justify-content-between w-100">
+                        <a href="details.php?id=<?php echo $pizza['id']; ?>" class="btn w-100 mr-1 btn-outline-primary text-uppercase">More Info</a>
+                       <form action="index.php" method="POST">
+                        <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+                        <input type="submit" name="delete" class="btn btn-outline-danger ml-1 text-uppercase w-100" value="Delete">
+                       </form>
                     </div>
                 </div>
             </div>
